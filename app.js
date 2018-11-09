c4041e7338aefff0ff60ebc6f7701c768e1e9b35
Laura Levenhagen <Laura@Lauras-MacBook-Pro.local>
2018-11-08 17:34:28 -0800

    changed a ton of css, added anchorscroll

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const app = angular.module('Portfolio', []);
 
-app.controller('MainController', ['$http', function($http){
+app.controller('MainController', ['$http', '$location', '$anchorScroll', function($http, $location, $anchorScroll){
 
+$location.hash('main')
+this.scrollTo = function(id) {
+  console.log('heyyy there');
+  $location.hash(id);
+  $anchorScroll();
+
+}
+
+
+
+
+
+this.showAbout = false;
 this.showLinked = false;
 this.showResume = false;
 this.showProject = false;
@@ -11,29 +24,40 @@ this.resumerModal = false;
 this.projectModal = false;
 this.favModal = false;
 
+this.showA = function(){
+  this.showAbout = !this.showAbout;
+  this.showLinked = false;
+  this.showResume = false;
+  this.showProject = false;
+  this.showGit = false;
+}
 this.showLinkedIn = function(){
   this.showLinked = !this.showLinked;
   this.showResume = false;
   this.showProject = false;
   this.showGit = false;
+  this.showAbout = false;
 }
 this.showR = function(){
   this.showLinked = false;
   this.showResume = !this.showResume;
   this.showProject = false;
   this.showGit = false;
+  this.showAbout = false;
 }
 this.showP = function(){
   this.showLinked = false;
   this.showResume = false;
   this.showProject = !this.showProject;
   this.showGit = false;
+  this.showAbout = false;
 }
 this.showGithub = function(){
   this.showLinked = false;
   this.showResume = false;
   this.showProject = false;
   this.showGit = !this.showGit;
+  this.showAbout = false;
 }
 this.toggleAboutModal = function(){
   this.aboutModal = !this.aboutModal;
