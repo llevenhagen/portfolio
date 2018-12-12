219cd71ea6f83900e71e69e13d59db26af1db338
Laura Levenhagen <Laura@Lauras-MacBook-Pro.local>
2018-12-11 18:26:38 -0800

    changed nav show depending on screen size

--- app.js
+++ app.js
@@ -12,64 +12,113 @@ this.scrollTo = function(id) {
 
 
 
+var intViewportWidth = document.body.clientWidth;
+console.log(intViewportWidth);
+let big = intViewportWidth < 970;
+let small = intViewportWidth >= 969;
+if (big) {
+  console.log('the screen is more than 760');
+  this.showAbout = false;
+  this.showLinked = false;
+  this.showResume = false;
+  this.showProject = false;
+  this.showGit = false;
+  this.aboutModal = false;
+  this.resumerModal = false;
+  this.projectModal = false;
+  this.favModal = false;
 
+  this.showA = function(){
+    this.showAbout = !this.showAbout;
+    this.showLinked = false;
+    this.showResume = false;
+    this.showProject = false;
+    this.showGit = false;
+  }
+  this.showLinkedIn = function(){
+    this.showLinked = !this.showLinked;
+    this.showResume = false;
+    this.showProject = false;
+    this.showGit = false;
+    this.showAbout = false;
+  }
+  this.showR = function(){
+    this.showLinked = false;
+    this.showResume = !this.showResume;
+    this.showProject = false;
+    this.showGit = false;
+    this.showAbout = false;
+  }
+  this.showP = function(){
+    this.showLinked = false;
+    this.showResume = false;
+    this.showProject = !this.showProject;
+    this.showGit = false;
+    this.showAbout = false;
+  }
+  this.showGithub = function(){
+    this.showLinked = false;
+    this.showResume = false;
+    this.showProject = false;
+    this.showGit = !this.showGit;
+    this.showAbout = false;
+  }
+} else if (small){
+  console.log('hey there this is less than 760');
+  this.showAbout = true;
+  this.showLinked = true;
+  this.showResume = true;
+  this.showProject = true;
+  this.showGit = true;
+}
+console.log(this.showAbout);
 
-this.showAbout = false;
-this.showLinked = false;
-this.showResume = false;
-this.showProject = false;
-this.showGit = false;
-this.aboutModal = false;
-this.resumerModal = false;
-this.projectModal = false;
-this.favModal = false;
-
-this.showA = function(){
-  this.showAbout = !this.showAbout;
-  this.showLinked = false;
-  this.showResume = false;
-  this.showProject = false;
-  this.showGit = false;
-}
-this.showLinkedIn = function(){
-  this.showLinked = !this.showLinked;
-  this.showResume = false;
-  this.showProject = false;
-  this.showGit = false;
-  this.showAbout = false;
-}
-this.showR = function(){
-  this.showLinked = false;
-  this.showResume = !this.showResume;
-  this.showProject = false;
-  this.showGit = false;
-  this.showAbout = false;
-}
-this.showP = function(){
-  this.showLinked = false;
-  this.showResume = false;
-  this.showProject = !this.showProject;
-  this.showGit = false;
-  this.showAbout = false;
-}
-this.showGithub = function(){
-  this.showLinked = false;
-  this.showResume = false;
-  this.showProject = false;
-  this.showGit = !this.showGit;
-  this.showAbout = false;
-}
-// this.toggleAboutModal = function(){
-//   this.aboutModal = !this.aboutModal;
-//   console.log('you clicked about');
+// this.showA = function(){
+//   this.showAbout = !this.showAbout;
+//   this.showLinked = false;
+//   this.showResume = false;
+//   this.showProject = false;
+//   this.showGit = false;
 // }
-// this.toggleResumeModal = function(){
-//   this.resumeModal = !this.resumeModal;
+// this.showLinkedIn = function(){
+//   this.showLinked = !this.showLinked;
+//   this.showResume = false;
+//   this.showProject = false;
+//   this.showGit = false;
+//   this.showAbout = false;
 // }
-// this.toggleProjectModal = function(){
-//   console.log('hello');
-//   this.projectModal = !this.projectModal;
+// this.showR = function(){
+//   this.showLinked = false;
+//   this.showResume = !this.showResume;
+//   this.showProject = false;
+//   this.showGit = false;
+//   this.showAbout = false;
 // }
+// this.showP = function(){
+//   this.showLinked = false;
+//   this.showResume = false;
+//   this.showProject = !this.showProject;
+//   this.showGit = false;
+//   this.showAbout = false;
+// }
+// this.showGithub = function(){
+//   this.showLinked = false;
+//   this.showResume = false;
+//   this.showProject = false;
+//   this.showGit = !this.showGit;
+//   this.showAbout = false;
+// }
+this.toggleAboutModal = function(){
+  this.aboutModal = !this.aboutModal;
+  console.log('you clicked about');
+}
+this.toggleResumeModal = function(){
+  this.resumeModal = !this.resumeModal;
+}
+this.toggleProjectModal = function(){
+  console.log('hello');
+  this.projectModal = !this.projectModal;
+}
 this.toggleFavModal = function(){
   this.favModal = !this.favModal;
 }
